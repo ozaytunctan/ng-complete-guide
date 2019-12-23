d58ed4ba3a4683e2bd751413ea367b44da2750fc
otunctan <otunctan@biletix.com>
2019-12-23 10:41:27 +0300

    dev branch created

--- src/app/service/recipe.service.ts
+++ src/app/service/recipe.service.ts
@@ -7,29 +7,33 @@ import { ShoppingListService } from './shopping-list.service';
   providedIn: 'root'
 })
 export class RecipeService {
-  
+
 
   selectedRecipe = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
-    new Recipe("A Test Recipe", 'This is simple a test', 'https://www.skinnytaste.com/wp-content/uploads/2009/02/turkey-meatloaf-8.jpg',
+    new Recipe("Börek", 'Bilice börek ',
+      'https://i4.hurimg.com/i/hurriyet/75/1500x844/5ab8c3f5c9de3d234835a917.jpg',
       [
-        new Ingredient("Küçük Boy", 5),
-        new Ingredient("Orta Boy", 10)
-      ]),
-    new Recipe("Cola", 'This is simple a cola', 'https://ayb.akinoncdn.com/products/2019/01/22/1485/4a19a83a-7901-456b-b12e-0b9798512b07.jpg',
-      [
-        new Ingredient("Cola", 6)
-      ]),
-    new Recipe("Çay", 'This is simple a tea', 'https://static.daktilo.com/sites/71/uploads/2019/10/01/cay-1569920968.jpg'
-      , [
-        new Ingredient("Dem", 8),
-        new Ingredient("Su", 5)
+        new Ingredient("3 adet Yumurta", 5),
+        new Ingredient("2 yemek kaşığı sıvı yağ", 1),
+        new Ingredient("5 adet yufka",10)
       ]
-    )
+      ),
+
+      new Recipe("Tost", 'Basit Tost Tarifi',
+      'https://i4.hurimg.com/i/hurriyet/75/1500x844/5c94ed86c03c0e3538e8b019.jpg',
+      [
+        new Ingredient("1 yumurta.", 1),
+        new Ingredient("3-4 dilim sucuk.", 2),
+        new Ingredient("4-5 dilim kaşar peyniri.",3),
+        new Ingredient("2 dilim tost ekmeği.",12)
+
+      ]
+      )    
   ];
 
-  constructor(private shoppingService:ShoppingListService) { }
+  constructor(private shoppingService: ShoppingListService) { }
 
   getRecipes() {
     return this.recipes.slice();
